0b4b2479115706a00f035629a40162cdb956e78a
Phat <22130206@st.hcmuaf.edu.vn>
2025-12-15 23:39:29 +0700

    add ui sidebar

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<div className="h-16 bg-[#222E35] flex items-center px-4 border-b border-[#222E35] select-none">
+
+		</div>
+	);
+};
+
+export default Header;
